7a5775a696a69ee2e4a4cb1adbc123c3b99fde56
EvanY <primehunter326@gmail.com>
2012-08-27 22:19:34 -0400

    Got the renderer working as specified
    Can draw sprites as specified by the opcode format.  The space+A_REVERSE trick didn't work for some reason, just printed everything black.  Using ACS_Block for now.

--- src/io.c
+++ src/io.c
@@ -4,14 +4,24 @@
 
 void printRow(byte row)
 {
-	int i;
+	byte i;
 	byte b;
 	for(i=0;i<8;i++){
 		b = (row>>(8-(i+1))) & 1;
 		printf("%d",b);
+		if(b == 0)
+			addch(' ');
+		else
+			addch(ACS_BLOCK);
 	}
 }
 
 void drawSprite(byte x, byte y, byte n, byte* s)
 {
+	byte i;
+	move(y,x);
+	for(i=0;i<n;i++){
+		move(y+i,x);
+		printRow(s[i]);
+	}
 }
--- src/tests/draw.c
+++ src/tests/draw.c
@@ -1,38 +1,50 @@
 #include <ncurses.h>
+#include "../io.h"
 
 
-
-int main()
-{
-	int row,col,y,x,yM,xM;
-	int i,j;	
-	yM = 32;
-	xM = 64;
-	initscr();
-	getmaxyx(stdscr,row,col);
-	for(i=0;i<yM;i++){
-		for(j=0;j<xM;j++){
-			mvaddch(i,j,(' ' | A_REVERSE));
-			//mvaddch(i,j,A_REVERSE);
-		}
-	}
-	getch();
-	endwin();
-	printf("rows: %d, cols: %d\n",row,col);
-	return 0;	
-}
-/*
 //test it
 int main()
 {
-	printRow(0xF0);
-	printf("\n");
-	printRow(0x90);
-	printf("\n");
-	printRow(0x90);
-	printf("\n");
-	printRow(0x90);
-	printf("\n");
-	printRow(0xF0);
-	printf("\n");
-}*/
+	byte two[5] = {0xf0,0x10,0xf0,0x80,0xf0};
+	byte one[5] = {0x20,0x60,0x20,0x20,0x70};
+	byte zero[5] = {0xf0,0x90,0x90,0x90,0xf0};
+	byte three[5] = {0xf0,0x10,0xf0,0x10,0xf0};
+	byte four[5] = {0x90,0x90,0xf0,0x10,0x10};
+	byte five[5] = {0xf0,0x80,0xf0,0x10,0xf0};
+	byte six[5] = {0xf0,0x80,0xf0,0x90,0xf0};
+	byte seven[5] = {0xf0,0x10,0x20,0x40,0x40};
+	byte eight[5] = {0xf0,0x90,0xf0,0x90,0xf0};
+	byte nine[5] = {0xf0,0x90,0xf0,0x10,0xf0};
+	byte A[5] = {0xf0,0x90,0xf0,0x90,0x90};
+	byte B[5] = {0xe0,0x90,0xe0,0x90,0xe0};
+	byte C[5] = {0xf0,0x80,0x80,0x80,0xf0};
+	byte D[5] = {0xe0,0x90,0x90,0x90,0xe0};
+	byte E[5] = {0xf0,0x80,0xf0,0x80,0xf0};
+	byte F[5] = {0xf0,0x80,0xf0,0x80,0x80};
+	byte smiley[5] = {0x18,0x24,0x42,0x81};	
+	
+	initscr();
+	//code
+	move(0,25);
+	printw("Printing some sprites!");
+	drawSprite(0,2,5,zero);
+	drawSprite(8,2,5,one);
+	drawSprite(16,2,5,two);
+	drawSprite(24,2,5,three);
+	drawSprite(32,2,5,four);
+	drawSprite(40,2,5,five);
+	drawSprite(48,2,5,six);
+	drawSprite(56,2,5,seven);
+	drawSprite(0,10,5,eight);
+	drawSprite(8,10,5,nine);
+	drawSprite(16,10,5,A);
+	drawSprite(24,10,5,B);
+	drawSprite(32,10,5,C);
+	drawSprite(40,10,5,D);
+	drawSprite(48,10,5,E);
+	drawSprite(56,10,5,F);
+	//drawSprite(28,15,4,smiley);
+	getch();
+	endwin();
+	return 0;
+}
